2cbed3461b9c30d19b9b0d92a5701d9f7305b6b3
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-06-23 14:34:18 +0200

    Basic support for postgresql

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,30 @@ import passport from 'passport'
 import session from 'express-session'
 import authRouter from './routes/authRouter'
 import indexRouter from './routes/indexRouter'
+import "reflect-metadata"
+import { createConnection } from "typeorm";
+import { User } from './entity/User'
 const Strategy = require('passport-google-oauth20').Strategy;
 
 dotenv.config()
 const app: express.Application = express()
 const PORT: string = process.env['PORT'] || '8080'
 
+const connectDB = async () => {
+    await createConnection().then(async connection => {
+        let delfi = new User()
+        delfi.id = "305"
+        delfi.name = "john smith"
+        await connection.manager.save(delfi)
+        console.log('saved')
+
+    })
+}
+
+connectDB()
 
 app.use(session({
-    secret: process.env['EXPRESS_SESSION_SECRET'] || process.exit(),
+    secret: process.env['EXPRESS_SESSION_SECRET'] || process.exit(1),
     resave: true,
     saveUninitialized: true,
 }))
